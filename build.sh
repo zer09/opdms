1094a63396e2bb7821e15203cdd5e08a19161242
Gilbert Consellado <gilbertconsellado@gmail.com>
2018-10-24 15:30:55 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # ionic cordova build browser --verbose && cp -r www/* ../opdms-server/public/
-ionic cordova build browser --verbose
+# ionic cordova build browser --verbose
+ionic build --prod --verbose && rm -rf ../opdms-server/public/* && cp -r www/* ../opdms-server/public/
