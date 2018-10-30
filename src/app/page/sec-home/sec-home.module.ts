d30f599cb7663f72526fdf8f29b01ddea4d4c8c6
Gilbert Consellado <gilbertconsellado@gmail.com>
2018-10-30 09:12:21 +0800

    Add patient searchbar on sec-home

--- src/app/page/sec-home/sec-home.module.ts
+++ src/app/page/sec-home/sec-home.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { ResponsiveModule } from 'ngx-responsive';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SecHomePage } from './sec-home.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: SecHomePage
-  }
+    {
+        path: '',
+        component: SecHomePage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [SecHomePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        ResponsiveModule.forRoot()
+    ],
+    declarations: [SecHomePage]
 })
-export class SecHomePageModule {}
+export class SecHomePageModule { }
